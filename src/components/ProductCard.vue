22c83d54bb9a07be7f99047c38c831054b3c0039
Saiyaf-Sdk <saiyaf.siddeek25@gmail.com>
2026-04-22 00:16:41 +0530

    Fix emit syntax in ProductCard.vue

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,19 +1,23 @@
 <script setup lang="ts">
-const emit = defineEmits(['select'])
+// Product type
 interface Product {
   id: number
   title: string
   price: number
 }
 
+// props
 defineProps<{
   product: Product
 }>()
+
+// emit
+const emit = defineEmits(['select'])
 </script>
 
 <template>
   <div 
-    @click="$emit('select', product)"
+    @click="emit('select', product)"
     class="border rounded p-4 shadow hover:shadow-lg cursor-pointer transition"
   >
     <h3 class="font-semibold text-lg">{{ product.title }}</h3>
