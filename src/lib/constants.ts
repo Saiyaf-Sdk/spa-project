e06df8ac64fc012ffd566dcbebac22d77b29af63
Saiyaf-Sdk <saiyaf.siddeek25@gmail.com>
2026-04-22 15:05:52 +0530

    Add constants for API and storage keys

---
+++ src/lib/constants.ts
@@ -0,0 +1,11 @@
+export const API_BASE_URL = 'https://dummyjson.com';
+
+export const AUTH_STORAGE_KEY = 'lumen-cart-auth';
+export const CART_STORAGE_KEY = 'lumen-cart-items';
+export const THEME_STORAGE_KEY = 'lumen-cart-theme';
+
+export const DEMO_USER = {
+  username: 'kminchelle',
+  password: '0lelplR'
+};
+
