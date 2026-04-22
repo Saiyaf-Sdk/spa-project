368c355a90bb75c209929dbbbc69d98928e4aedd
Saiyaf-Sdk <saiyaf.siddeek25@gmail.com>
2026-04-22 14:41:31 +0530

    Remove cart functionality from store.ts
    Removed the entire cart functionality including the Product interface and useCart function.

--- store.ts
+++ store.ts
@@ -1,17 +1,1 @@
-import { ref } from 'vue'
 
-interface Product {
-  id: number
-  title: string
-  price: number
-}
-
-const cart = ref<Product[]>([])
-
-export function useCart() {
-  const addToCart = (product: Product) => {
-    cart.value.push(product)
-  }
-
-  return { cart, addToCart }
-}
